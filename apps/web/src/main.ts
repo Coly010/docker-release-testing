933e521783b7e02f6eec193f729179be6bc406ae
Colum Ferry <cferry09@gmail.com>
2025-07-02 16:27:59 +0100

    feat: use shared-utils library

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,5 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import {sharedUtils} from '@org/shared-utils';
+
+console.log("sharedUtils", sharedUtils());
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
